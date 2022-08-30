b15d665b551f3cf8b9eaa80cd249dc2e89b438d6
acer <acer@DESKTOP-V9361ET>
2022-08-30 13:38:11 +0530

    first commit.....

--- src/com/git/git1.java
+++ src/com/git/git1.java
@@ -2,4 +2,12 @@ package com.git;
 
 public class git1 {
 
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry =="yes")
+			System.out.println("Eat Something");
+		else
+		System.out.println("Do Work");
+	}
+
 }
